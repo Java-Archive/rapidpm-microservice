25d2813909bfa47d87d41b833f6c684433f03721
Sven Ruppert <sven.ruppert@gmail.com>
2016-04-06 15:38:58 +0200

    small updates

--- modules/core/src/main/java/org/rapidpm/microservice/optionals/header/HeaderScreenPrinter.java
+++ modules/core/src/main/java/org/rapidpm/microservice/optionals/header/HeaderScreenPrinter.java
@@ -21,7 +21,6 @@ package org.rapidpm.microservice.optionals.header;
 
 import org.rapidpm.ddi.DI;
 
-import java.util.Optional;
 import java.util.Set;
 import java.util.function.Function;
 
@@ -40,36 +39,20 @@ public class HeaderScreenPrinter {
               "         /_/                                                                                \n";
       System.out.println(txt);
       System.out.println("");
-      System.out.println(" this project is hosted at https://github.com/RapidPM/rapidpm-microservice");
-      System.out.println(" examples you can find at  https://github.com/RapidPM/rapidpm-microservice-examples");
+      System.out.println(" this project is hosted at https://github.com/orgs/JavaMicroService/dashboard");
+      System.out.println(" examples you can find at  https://github.com/orgs/JavaMicroService/rapidpm-microservice-examples");
       System.out.println(" if you have question      mailto:sven.ruppert@gmail.com or Twitter: @SvenRuppert");
       System.out.println("");
     } else {
       typesAnnotatedWith
           .stream()
           .filter(c -> c.isAnnotationPresent(Header.class))
-          .sorted((c1, c2) -> {
-            int c1Order = Integer.MIN_VALUE;
-            int c2Order = Integer.MIN_VALUE;
-
-            if (c1.isAnnotationPresent(Header.class)) {
-              c1Order = c1.getAnnotation(Header.class).order();
-            }
-            if (c2.isAnnotationPresent(Header.class)) {
-              c2Order = c2.getAnnotation(Header.class).order();
-            }
-            return Integer.compare(c1Order, c2Order);
-          })
-          .map((Function<Class<? extends HeaderInfo>, Optional<HeaderInfo>>) aClass -> {
-            try {
-              return Optional.of(DI.activateDI(aClass.newInstance()));
-            } catch (InstantiationException | IllegalAccessException e) {
-              e.printStackTrace();
-            }
-            return Optional.empty();
-          })
-          .filter(Optional::isPresent)
-          .map(Optional::get)
+          .sorted((c1, c2) -> Integer
+              .compare(
+                  c1.getAnnotation(Header.class).order(),
+                  c2.getAnnotation(Header.class).order()))
+          .map((Function<Class<? extends HeaderInfo>, HeaderInfo>) DI::activateDI)
+          .filter(h -> h != null)
           .map(HeaderInfo::createHeaderInfo)
           .forEach(System.out::println);
     }
