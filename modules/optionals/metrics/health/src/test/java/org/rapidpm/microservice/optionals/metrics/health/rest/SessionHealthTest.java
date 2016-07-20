7e990a40827bf372c6972b846ff8cdc24b877968
merodwin <benjamin.bosch@merodwin.de>
2016-07-20 08:05:50 +0200

    moved debug output to better place

--- modules/optionals/metrics/health/src/test/java/org/rapidpm/microservice/optionals/metrics/health/rest/SessionHealthTest.java
+++ modules/optionals/metrics/health/src/test/java/org/rapidpm/microservice/optionals/metrics/health/rest/SessionHealthTest.java
@@ -40,128 +40,128 @@ import java.net.URL;
 import java.util.List;
 
 
-public class SessionHealthTest  {
+public class SessionHealthTest {
 
-    private static String url;
-    private final String USER_AGENT = "Mozilla/5.0";
+  private static String url;
+  private final String USER_AGENT = "Mozilla/5.0";
 
-    @BeforeClass
-    public static void setUpClass() {
-        final PortUtils portUtils = new PortUtils();
-        System.setProperty(Main.REST_PORT_PROPERTY, portUtils.nextFreePortForTest() + "");
-        System.setProperty(Main.SERVLET_PORT_PROPERTY, portUtils.nextFreePortForTest() + "");
-        url = "http://127.0.0.1:" + System.getProperty(Main.SERVLET_PORT_PROPERTY) + Main.MYAPP + "/test"; //from Annotation Servlet
-        System.out.println("url = " + url);
+  @BeforeClass
+  public static void setUpClass() {
+    final PortUtils portUtils = new PortUtils();
+    System.setProperty(Main.REST_PORT_PROPERTY, portUtils.nextFreePortForTest() + "");
+    System.setProperty(Main.SERVLET_PORT_PROPERTY, portUtils.nextFreePortForTest() + "");
+    url = "http://127.0.0.1:" + System.getProperty(Main.SERVLET_PORT_PROPERTY) + Main.MYAPP + "/test"; //from Annotation Servlet
+    System.out.println("url = " + url);
+  }
+
+  @Before
+  public void startUp() {
+    DI.clearReflectionModel();
+    DI.activatePackages("org.rapidpm");
+    DI.activatePackages("junit.org.rapidpm");
+    Main.deploy();
+  }
+
+  @After
+  public void tearDown() {
+    Main.stop();
+    DI.clearReflectionModel();
+  }
+
+  @Test
+  public void heathTest001() {
+    final String generateBasicReqURL = generateBasicReqURL(SessionHealth.class);
+    Client client = ClientBuilder.newClient();
+    System.out.println("generateBasicReqURL = " + generateBasicReqURL);
+    final Builder authcode = client
+            .target(generateBasicReqURL)
+            .request();
+    final Response response = authcode.get();
+
+    Assert.assertEquals(200, response.getStatus());
+    String val = response.getStatusInfo().toString();
+    System.out.println("response status info = " + val);
+    client.close();
+
+  }
+
+  public String generateBasicReqURL(Class restClass) {
+    final String restAppPath = Main.CONTEXT_PATH_REST;
+    return new RestUtils().generateBasicReqURL(restClass, restAppPath);
+  }
+
+  @Test
+  public void heathTest002() {
+    final String generateBasicReqURL = generateBasicReqURL(SessionHealth.class);
+    Client client = ClientBuilder.newClient();
+    System.out.println("generateBasicReqURL = " + generateBasicReqURL);
+    final Builder authcode = client
+            .target(generateBasicReqURL)
+            .request();
+    String response = authcode.get(String.class);
+    Gson gson = new Gson();
+    SessionHealthInfo[] sessionHealthInfo = gson.fromJson(response, SessionHealthInfo[].class);
+
+    Assert.assertEquals(0L, sessionHealthInfo[0].activeSessionCount);
+
+    client.close();
+
+  }
+
+  @Test
+  public void heathTest003() throws IOException {
+    generateSession();
+    generateSession();
+    generateSession();
+
+    String url = generateBasicReqURL(SessionHealth.class);
+    URL obj = new URL(url);
+    System.out.println("\nSending 'GET' request to URL : " + url);
+    HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+    // optional default is GET
+    con.setRequestMethod("GET");
+    //add request header
+    con.setRequestProperty("User-Agent", USER_AGENT);
+
+    int responseCode = con.getResponseCode();
+    System.out.println("Response Code : " + responseCode);
+
+    BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+    String inputLine;
+    StringBuffer response = new StringBuffer();
+
+    while ((inputLine = in.readLine()) != null) {
+      response.append(inputLine);
     }
+    in.close();
 
-    @Before
-    public void startUp(){
-        DI.clearReflectionModel();
-        DI.activatePackages("org.rapidpm");
-        DI.activatePackages("junit.org.rapidpm");
-        Main.deploy();
+    final String jsonResponse = response.toString();
+    Assert.assertFalse(jsonResponse.isEmpty());
+
+    final List<SessionHealthInfo> healthInfos = new SessionHealthInfoJsonConverter().fromJsonList(jsonResponse);
+    Assert.assertEquals(3L, healthInfos.get(0).activeSessionCount);
+  }
+
+  public void generateSession() throws IOException {
+
+    URL obj = new URL(url);
+    System.out.println("\nSending 'GET' request to URL : " + url);
+    HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+    // optional default is GET
+    con.setRequestMethod("GET");
+    //add request header
+    con.setRequestProperty("User-Agent", USER_AGENT);
+
+    int responseCode = con.getResponseCode();
+    System.out.println("Response Code : " + responseCode);
+
+    BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+    String inputLine;
+    StringBuffer response = new StringBuffer();
+
+    while ((inputLine = in.readLine()) != null) {
+      response.append(inputLine);
     }
-    
-    @After
-    public void tearDown(){
-        Main.stop();
-        DI.clearReflectionModel();
-    }
-    
-    @Test
-    public void heathTest001(){
-        final String generateBasicReqURL = generateBasicReqURL(SessionHealth.class);
-        Client client = ClientBuilder.newClient();
-        System.out.println("generateBasicReqURL = " + generateBasicReqURL);
-        final Builder authcode = client
-                .target(generateBasicReqURL)
-                .request();
-        final Response response = authcode.get();
-
-        Assert.assertEquals(200, response.getStatus());
-        String val = response.getStatusInfo().toString();
-        System.out.println("response status info = " + val);
-        client.close();
-        
-    }
-
-    public String generateBasicReqURL(Class restClass) {
-        final String restAppPath = Main.CONTEXT_PATH_REST;
-        return new RestUtils().generateBasicReqURL(restClass, restAppPath);
-    }
-
-    @Test
-    public void heathTest002(){
-        final String generateBasicReqURL = generateBasicReqURL(SessionHealth.class);
-        Client client = ClientBuilder.newClient();
-        System.out.println("generateBasicReqURL = " + generateBasicReqURL);
-        final Builder authcode = client
-                .target(generateBasicReqURL)
-                .request();
-        String response= authcode.get(String.class);
-        Gson gson = new Gson();
-        SessionHealthInfo[] sessionHealthInfo = gson.fromJson(response,SessionHealthInfo[].class );
-
-        Assert.assertEquals(0L,sessionHealthInfo[0].activeSessionCount);
-
-        client.close();
-
-    }
-
-    @Test
-    public void heathTest003() throws IOException {
-        generateSession();
-        generateSession();
-        generateSession();
-
-        String url = generateBasicReqURL(SessionHealth.class);
-        URL obj = new URL(url);
-        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-        // optional default is GET
-        con.setRequestMethod("GET");
-        //add request header
-        con.setRequestProperty("User-Agent", USER_AGENT);
-
-        int responseCode = con.getResponseCode();
-        System.out.println("\nSending 'GET' request to URL : " + url);
-        System.out.println("Response Code : " + responseCode);
-
-        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-        String inputLine;
-        StringBuffer response = new StringBuffer();
-
-        while ((inputLine = in.readLine()) != null) {
-            response.append(inputLine);
-        }
-        in.close();
-
-        final String jsonResponse = response.toString();
-        Assert.assertFalse(jsonResponse.isEmpty());
-
-        final List<SessionHealthInfo> healthInfos = new SessionHealthInfoJsonConverter().fromJsonList(jsonResponse);
-        Assert.assertEquals(3L,healthInfos.get(0).activeSessionCount);
-    }
-
-    public void generateSession() throws IOException {
-
-        URL obj = new URL(url);
-        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-        // optional default is GET
-        con.setRequestMethod("GET");
-        //add request header
-        con.setRequestProperty("User-Agent", USER_AGENT);
-
-        int responseCode = con.getResponseCode();
-        System.out.println("\nSending 'GET' request to URL : " + url);
-        System.out.println("Response Code : " + responseCode);
-
-        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-        String inputLine;
-        StringBuffer response = new StringBuffer();
-
-        while ((inputLine = in.readLine()) != null) {
-            response.append(inputLine);
-        }
-        in.close();
-    }
+    in.close();
+  }
 }
