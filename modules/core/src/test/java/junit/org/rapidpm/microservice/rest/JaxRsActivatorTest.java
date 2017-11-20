9bfef3a97e11bcb7b86c3592f8dc03b50cc3f53c
dve <dve@vergien.net>
2017-11-20 08:44:35 +0100

    Add method to get all types with `@Path` annotations

--- modules/core/src/test/java/junit/org/rapidpm/microservice/rest/JaxRsActivatorTest.java
+++ modules/core/src/test/java/junit/org/rapidpm/microservice/rest/JaxRsActivatorTest.java
@@ -4,6 +4,8 @@ package junit.org.rapidpm.microservice.rest;
 import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 import java.util.Set;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
 import org.junit.Before;
 import org.junit.Test;
 import org.rapidpm.ddi.DI;
@@ -19,14 +21,44 @@ public class JaxRsActivatorTest {
     DI.clearReflectionModel();
     final Class<?> aClass = ProviderImpl.class;
     DI.activatePackages(aClass);
+    DI.activatePackages(InterfaceResource.class);
   }
 
   @Test
   public void testGetClasses() {
-
     Set<Class<?>> classes = jaxRsActivator.getClasses();
     assertThat(classes.contains(PingMe.class), is(true));
     assertThat(classes.contains(ProviderImpl.class), is(true));
+    assertThat(classes.contains(InterfaceResource.class), is(false));
   }
 
+  @Test
+  public void testGetPathResources() {
+    Set<Class<?>> classes = jaxRsActivator.getPathResources();
+    assertThat(classes.contains(PingMe.class), is(true));
+    assertThat(classes.contains(ProviderImpl.class), is(false));
+    assertThat(classes.contains(InterfaceResource.class), is(true));
+  }
+
+  @Test
+  public void testGetInterfacesWithPathAnnotation() {
+    Set<Class<?>> classes = jaxRsActivator.getInterfacesWithPathAnnotation();
+    assertThat(classes.contains(PingMe.class), is(false));
+    assertThat(classes.contains(ProviderImpl.class), is(false));
+    assertThat(classes.contains(InterfaceResource.class), is(true));
+
+  }
+
+  @Path("/interfaceResource")
+  public static interface InterfaceResource {
+    @GET
+    public String hello();
+  }
+
+  public static class InterfaceResourceImpl implements InterfaceResource {
+    @Override
+    public String hello() {
+      return "Hello, World!";
+    }
+  }
 }
